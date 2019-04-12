2d5697f7287efc2bcedcaa01c136962b0dbd8dd1
Rémy Tomasetto <remy.tomasetto@al-enterprise.com>
2019-04-12 18:58:36 +0200

    start buddy system c++14.

---
+++ include/buddy.hh
@@ -0,0 +1,89 @@
+#pragma once
+/*
+ * buddy allocator oriented to parser:lexer.
+ *
+ * Remy tomasetto 2019.
+ *
+ */
+
+#include <stdint>
+#include <vector>
+
+// template <typename ??>
+class buddy_allocator {
+  // Explicite construtor
+  // No copy-constructors.
+public:
+ explicit buddy_allocator(const T& t_value) // explicit means no implicit conversion.
+    : value(t_value) {}
+  buddy_allocator(const buddy_allocator&)            = delete;
+  buddy_allocator& operator=(const buddy_allocator&) = delete;
+  // the destructor does not clean the pieces ; it is sometimes not necessary to do that if the process ends just after.
+  // the clean must be explicit using valgrind kind of tools.
+  ~buddy_allocator()                                 = default;
+
+
+public:
+  void
+  clean ()
+    {
+      std::for_each (pieces.rbegin(), pieces.rend(), /* lambda(x) delete x*/ &(::operator delete));
+      pieces.clear ();
+      begin_of_piece = nullptr;
+      end_of_piece   = nullptr;
+    }
+
+  template <typename T>
+  inline T *  get_mem()
+  {
+    return static_cast<T *>(buddy_get(sizeof (T)));
+  }
+  template <typename T>
+  inline T *  get_array_mem(std::size_t dim)
+  {
+    return static_cast<T *>(buddy_get(sizeof (T) * dim));
+  }
+private:
+  std::vector<std::uintptr_t> pieces         {};
+
+  std::uintptr_t             *begin_of_piece {nullptr};
+  std::uintptr_t                *end_of_piece   {nullptr};
+
+  std::uintptr_t
+  buddy_get(std::size_t t_size)
+  {
+
+    const diffptr_t amount_of_free_space = end_of_piece - begin_of_piece;
+    if (amount_of_free_space > 0)
+      {
+	if (t_size <=  amount_of_free_space)
+	  {
+	    std::uintptr_t a_small_piece {begin_of_piece};
+	    begin_of_piece += t_size;
+	    return a_small_piece;
+	  }
+	else  if (t_size > BUDDY_SIZE)
+	  {
+	    std::byte *a_big_piece { new std::byte[t_size]};
+	    pieces.push_back(a_big_piece);
+	    return a_big_piece;
+	  }
+	else
+	  {
+	    std::byte *new_piece { new std::byte[BUDDY_SIZE]};
+	    begin_of_piece = new_piece;
+	    end_of_piece = new_piece +BUDDY_SIZE;
+	    pieces.push_back(new_piece);
+	  }
+      }
+    else
+      {
+	std::byte *new_piece { new std::byte[BUDDY_SIZE]};
+	begin_of_piece = new_piece;
+	end_of_piece = new_piece +BUDDY_SIZE;
+	pieces.push_back(new_piece);
+      }
+
+A revoir les new.
+
+};
--- src/rnd.cc
+++ src/rnd.cc
@@ -2,8 +2,83 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * @brief Solution using a bitmask to mark the seen characters.
+ * @return true if the input string has duplicate characters, false otherwise.
+ * @note Complexity: O(n) in time, O(1) in space, where n is the string length.
+ * @note This implementation assumes all characters are in the range [a-z].
+ */
+bool has_duplicates_1(const std::string& str)
+{
+    unsigned int chars_seen = 0;
 
+    for (const char c : str)
+    {
+        /* if the character c has already been seen */
+        if (chars_seen & (1 << (c - 'a')))
+        {
+            return true;
+        }
+        chars_seen |= 1 << (c - 'a');
+    }
 
+    return false;
+}
+/**
+ * @brief Solution using a bitmask to track seen characters (the input string is
+ *        allowed to contain any valid ASCII characters).
+ * @return A reference to the input string with all duplicate characters
+ *         removed.
+ * @note Complexity: O(n) in time, O(1) in space, where n is the string length.
+ */
+std::string& remove_duplicates_2(std::string& str)
+{
+    if (str.size() <= 1)
+    {
+        return str;
+    }
+
+    std::bitset<128> chars_seen{};
+
+    /* next writing position (the first character is skipped) */
+    size_t i = 1;
+
+    /* the first character is marked as seen and skipped */
+    chars_seen[str[0]] = true;
+
+    for (size_t j = 1; j < str.size(); ++j)
+    {
+        if (chars_seen[str[j]] == false)
+        {
+            chars_seen[str[j]] = true;
+
+            str[i] = str[j];
+            ++i;
+        }
+    }
+
+    /* all characters in str[i..str.size()) are duplicates, discard them */
+    str.resize(i);
+    str.shrink_to_fit();
+
+    return str;
+}
+
+/**
+ * @brief Solution which checks if str2 appears in (str1 + str1).
+ * @return true if str2 is a rotation of str1, false otherwise.
+ * @note Complexity: O(n) in both time and space, where n is the length of
+ *       the strings (if not the same, one cannot be a rotation of the other).
+ */
+bool is_rotation_1(const std::string& str1, const std::string& str2)
+{
+    if (str1.size() != str2.size())
+    {
+        return false;
+    }
+
+    return (str1 + str1).find(str2) != std::string::npos;
+}
 int
 main (int argc, char **argv)
 {
@@ -11,7 +86,7 @@ main (int argc, char **argv)
 
   mR = 1;
   mS = mB = 0;
-	
+
   for (int i =0; i < 1024*1024*128; i++)
     {
       mR = mR * 5;
@@ -20,5 +95,5 @@ main (int argc, char **argv)
       mB = mS & 0x7f;
       if ((i % 0x8000) == 0)
         printf(" R[%d] = %08x -- S(%d) B(%d)\n", i, mR, mS, mB );
-    } 
+    }
 }
